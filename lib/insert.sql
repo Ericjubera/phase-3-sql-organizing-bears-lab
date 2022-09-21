17045239a10672c9ffe96098245c87de79b1ff72
eric jubera <ericjubera@gmail.com>
2022-09-21 11:33:59 -0500

    commit tree 7548e73eebdf7a9e6e9482014248976cf3d88150

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears VALUES (1,"Mr. Chocolate",22,"M","red","calm",1);
+INSERT INTO bears VALUES (2,"Rowdy",27,"M","red","calm",1);
+INSERT INTO bears VALUES (3,"Tabitha",12,"M","red","calm",1);
+INSERT INTO bears VALUES (4,"Sergeant Brown",23,"F","red","calm",1);
+INSERT INTO bears VALUES (5,"Melissa",2,"F","red","calm",1);
+INSERT INTO bears VALUES (6,"Grinch",14,"M","red","calm",1);
+INSERT INTO bears VALUES (7,"Wendy",6,"F","red","calm",1);
+INSERT INTO bears VALUES (8,NULL,7,"M","red","calm",1);
